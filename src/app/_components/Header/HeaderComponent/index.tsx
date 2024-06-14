8631e14ff4a5cf5b7707467f422441f70222200c
Advaita <148793147+01adv@users.noreply.github.com>
2024-06-14 16:52:30 +0530

    done till footer

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,30 @@
+'use client'
+import Link from 'next/link'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+
+import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { noHeaderFooterUrls } from '../../../constants'
+import { HeaderNav } from '../Nav'
+import classes from './index.module.scss'
+
+const HeaderComponent = ({ header }: { header: Header }) => {
+  const pathname = usePathname()
+  return (
+    <nav
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Gutter className={classes.wrap}>
+        <Link href="/">
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
+        </Link>
+        <HeaderNav header={header} />
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
